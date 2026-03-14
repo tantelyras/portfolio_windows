b8d704cb2182e0a26de2eed3c2a68d2ab1d3a6d7
Tantelinirina RASOLONIAINA <rasoloniaina@boa.mg>
2026-03-14 13:53:58 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
   }
 })
